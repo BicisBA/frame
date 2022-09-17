50b8585cbc57781132231e3486035830a7790900
CrossNox <ijmermet@gmail.com>
2022-09-17 17:32:34 -0300

    remove hardcoded secret

--- frame/constants.py
+++ frame/constants.py
@@ -5,7 +5,6 @@ import pathlib
 
 DEFAULT_SQLITE_LOC = pathlib.Path(__name__).parent.parent / "frame.db"
 DEFAULT_SQLITE = f"sqlite:////{DEFAULT_SQLITE_LOC.resolve()}"
-DEFAULT_API_SECRET = "wH*0#m$l`4v7e'HF;Hez'6fhNuthKH`Eg|#[<5FILs19}<P@"
 
 
 class Environments(enum.Enum):
