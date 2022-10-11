6738a7902fcb7199841390734945465bb1548430
CrossNox <ijmermet@gmail.com>
2022-10-11 00:32:36 -0300

    fix psql dialect

--- frame/models/base.py
+++ frame/models/base.py
@@ -9,7 +9,15 @@ from frame.constants import DEFAULT_SQLITE
 
 logger = get_logger(__name__)
 
-DB_URL = cfg.database.url(default=DEFAULT_SQLITE)
+
+def fix_psql_schema(db_conn_url: str) -> str:
+    """Fix the deprecated postgres dialect."""
+    if db_conn_url.startswith("postgres://"):
+        db_conn_url = db_conn_url.replace("postgres://", "postgresql+psycopg2://")
+    return db_conn_url
+
+
+DB_URL = cfg.database.url(default=DEFAULT_SQLITE, cast=fix_psql_schema)
 DB_CONN_ARGS = {"check_same_thread": False} if DB_URL == DEFAULT_SQLITE else {}
 
 engine = create_engine(DB_URL, connect_args=DB_CONN_ARGS)
