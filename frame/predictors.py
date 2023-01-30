7f35a9f8bccf7e72d5cf46a3de8e07055ba00344
flopezd <fdestain@fi.uba.ar>
2023-01-29 22:34:41 -0300

    Fix eta feature. Use text file for current models.

--- frame/predictors.py
+++ frame/predictors.py
@@ -6,7 +6,14 @@ from abc import ABC, abstractmethod
 import joblib
 
 from frame.s3 import S3Client
+from frame.utils import get_logger
 from frame.models import StationStatus
+
+logger = get_logger(__name__)
+
+BUCKET = "frame"
+CURRENT_AVAILABILITY_KEY = "models/current_availability_model.txt"
+CURRENT_ETA_KEY = "models/current_eta_model.txt"
 
 
 class AvailabilityPredictor(ABC):
@@ -29,8 +36,8 @@ class ETAPredictor(ABC):
         """Returns the expected number of minutes for a bicycle arrival.
 
         Args:
-            features (List[List]): [["hour", "dow", "num_bikes_available",
-            "num_bikes_disabled", "num_docks_available", "num_docks_disabled"]]
+            features (List[List]): [["hour", "dow", "num_bikes_disabled",
+            "num_docks_available", "num_docks_disabled"]]
         Returns:
             List: number of minutes for a bicycle arrival
         """
@@ -57,7 +64,9 @@ class Predictors:
     _instance = None
     _s3_client: S3Client
     _availability_predictor: AvailabilityPredictor
+    _availability_predictor_key: str = ""
     _eta_predictor: ETAPredictor
+    _eta_predictor_key: str = ""
 
     def __new__(cls):
         if cls._instance is None:
@@ -66,19 +75,48 @@ class Predictors:
         return cls._instance
 
     def reload_models(self):
-        # TODO: include one model per station
-        with BytesIO() as mem_f:
-            self._s3_client.client.Bucket("frame").download_fileobj(
-                Key="models/current_availability_model.joblib", Fileobj=mem_f
+        last_availability_predictor_key = (
+            self._s3_client.client.Bucket(BUCKET)
+            .Object(CURRENT_AVAILABILITY_KEY)
+            .get()["Body"]
+            .read()
+            .decode()
+        )
+        if self._availability_predictor_key != last_availability_predictor_key:
+            with BytesIO() as mem_f:
+                self._s3_client.client.Bucket(BUCKET).download_fileobj(
+                    Key=last_availability_predictor_key, Fileobj=mem_f
+                )
+                mem_f.seek(0)
+                self._availability_predictor = joblib.load(mem_f)
+            self._availability_predictor_key = last_availability_predictor_key
+            logger.info(
+                "New availability model loaded '%s'", last_availability_predictor_key
             )
-            mem_f.seek(0)
-            self._availability_predictor = joblib.load(mem_f)
-        with BytesIO() as mem_f:
-            self._s3_client.client.Bucket("frame").download_fileobj(
-                Key="models/current_eta_model.joblib", Fileobj=mem_f
+        else:
+            logger.info(
+                "Current availability model '%s' is up to date!",
+                last_availability_predictor_key,
             )
-            mem_f.seek(0)
-            self._eta_predictor = joblib.load(mem_f)
+
+        last_eta_predictor_key = (
+            self._s3_client.client.Bucket(BUCKET)
+            .Object(CURRENT_ETA_KEY)
+            .get()["Body"]
+            .read()
+            .decode()
+        )
+        if self._eta_predictor_key != last_eta_predictor_key:
+            with BytesIO() as mem_f:
+                self._s3_client.client.Bucket(BUCKET).download_fileobj(
+                    Key=last_eta_predictor_key, Fileobj=mem_f
+                )
+                mem_f.seek(0)
+                self._eta_predictor = joblib.load(mem_f)
+            self._eta_predictor_key = last_eta_predictor_key
+            logger.info("New eta model loaded '%s'", last_eta_predictor_key)
+        else:
+            logger.info("Current eta model '%s' is up to date!", last_eta_predictor_key)
 
     def predict_availability(
         self,
@@ -106,7 +144,6 @@ class Predictors:
             [
                 eta_features.hour,
                 eta_features.dow,
-                eta_features.station_status.num_bikes_available,
                 eta_features.station_status.num_bikes_disabled,
                 eta_features.station_status.num_docks_available,
                 eta_features.station_status.num_docks_disabled,
