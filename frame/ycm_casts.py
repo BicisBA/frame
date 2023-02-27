8347e67a968abec112bcdba0903170a022ca329a
CrossNox <ijmermet@gmail.com>
2023-02-27 04:13:37 -0300

    fix

--- frame/ycm_casts.py
+++ frame/ycm_casts.py
@@ -5,6 +5,7 @@ from frame.cert import download_cert
 def s3_or_local(path: str):
     if not path.startswith("s3://"):
         return path
+    path = path[len("s3://") :]
     return download_cert(
         cfg.s3.endpoint_url(),
         cfg.s3.access_key(),
