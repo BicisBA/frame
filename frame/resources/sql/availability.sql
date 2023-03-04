82885d3f6ce0167a9763116c57e9b5d30d517ac4
CrossNox <ijmermet@gmail.com>
2023-03-04 20:24:46 -0300

    restore column

--- frame/resources/sql/availability.sql
+++ frame/resources/sql/availability.sql
@@ -8,12 +8,20 @@ SELECT
     num_docks_disabled,
     status,
 {% for i in range(1, 7) %}
+    minute(lead(make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0), {{i}}) over (
+        partition by station_id
+        order by make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0) asc
+    ) - make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0)) as minutes_bt_check_{{i}},
     (lead(num_bikes_available, {{i}}) over (
         partition by station_id
         order by make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0) asc
     ) > 0)::int as bikes_available_{{i}},
 {% endfor %}
 {% for i in range(7,18,3) %}
+    minute(lead(make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0), {{i}}) over (
+        partition by station_id
+        order by make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0) asc
+    ) - make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0)) as minutes_bt_check_{{i}},
     (lead(num_bikes_available, {{i}}) over (
         partition by station_id
         order by make_timestamp(year::int, month::int, day::int, hour::int, minute::int, 0.0) asc
