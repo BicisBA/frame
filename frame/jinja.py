df97fbfe41c8a63799e42a3cce42521689518675
CrossNox <ijmermet@gmail.com>
2023-03-04 21:20:58 -0300

    error when missing variable

--- frame/jinja.py
+++ frame/jinja.py
@@ -49,21 +49,6 @@ between {_make_timestamp(start_date)}
 and {_make_timestamp(end_date)} - INTERVAL 1 SECOND"""
 
 
-def filter_dates(start_date: date, end_date: date) -> str:
-    """Filter dates using each column."""
-
-    # Is start_date <= year-month-day <= end_date?
-    return f"""(
-    ({start_date.year} < year) or
-    ({start_date.year} = year and {start_date.month} < month) or
-    ({start_date.year} = year and {start_date.month} = month and {start_date.day} <= day)
-) and (
-    (year < {end_date.year}) or
-    (year = {end_date.year} and month < {end_date.month}) or
-    (year = {end_date.year} and month = {end_date.month} and day <= {end_date.day})
-)"""
-
-
 def load_sql_query(filename: str) -> str:
     """Load a SQL query from package resources.
 
@@ -96,13 +81,10 @@ def render_sql_query(sql: str, **query_context_params) -> str:
     sql : str
         Formatted query.
     """
-    jinja_logging_undef = jinja2.make_logging_undefined(
-        logger=logger, base=jinja2.Undefined
-    )
     env = jinja2.Environment(
         trim_blocks=True,
         lstrip_blocks=True,
-        undefined=jinja_logging_undef,
+        undefined=jinja2.StrictUndefined,
         autoescape=True,
     )
     env.filters["wrap"] = wrap
