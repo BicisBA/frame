c88b798540febbbd5589dd2b01271982c0c0a754
CrossNox <ijmermet@gmail.com>
2023-03-06 21:41:40 -0300

    WIP: Better ETA Query (#15)
    * fixes
    
    * fixes
    
    * fixes
    
    * add mape
    
    * bump

--- frame/train/eta.py
+++ frame/train/eta.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from typing import List, Tuple, Optional
+from typing import Tuple, Optional
 
-import pandas as pd
 from lightgbm import LGBMRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
@@ -24,6 +23,7 @@ logger = get_logger(__name__)
 
 
 ETA_NUM_FEATURES: Tuple[str, ...] = (
+    "num_bikes_available",
     "num_bikes_disabled",
     "num_docks_available",
     "num_docks_disabled",
@@ -33,35 +33,8 @@ ETA_CAT_FEATURES: Tuple[str, ...] = ("hod", "dow")
 
 PARTITION_COLUMN: str = "station_id"
 
-ETA_TARGET: str = "minutes_bt_check"
-ETA_METRICS: Tuple[FrameMetric, ...] = (FrameMetric.MAE,)
-
-DEFAULT_MINUTES_TO_EVAL_ETA: List[int] = list(range(1, 7)) + list(range(7, 18, 3))
-
-
-def postprocess_dataset_eta(dataset: pd.DataFrame) -> pd.DataFrame:
-    dataset["id"] = dataset.index
-    dataset = (
-        pd.wide_to_long(
-            dataset,
-            stubnames=["remaining_bikes_available", "minutes_bt_check"],
-            i="id",
-            j="temp_j",
-            sep="_",
-            suffix=r"\d+",
-        )
-        .dropna()
-        .reset_index(drop=True)
-    )
-    dataset["remaining_bikes_available"] = dataset["remaining_bikes_available"].astype(
-        "uint8"
-    )
-    dataset["minutes_bt_check"] = dataset["minutes_bt_check"].astype("uint8")
-    dataset = dataset[
-        (dataset["remaining_bikes_available"] > dataset["num_bikes_available"])
-        & (dataset["num_bikes_available"] <= 2)
-    ]
-    return dataset
+ETA_TARGET: str = "minutes_until_next_bike_arrival"
+ETA_METRICS: Tuple[FrameMetric, ...] = (FrameMetric.MAE, FrameMetric.MAPE)
 
 
 def train_eta(
@@ -75,7 +48,6 @@ def train_eta(
     test_size: float = DEFAULT_TEST_SIZE,
     partition_column: str = PARTITION_COLUMN,
     env: Environments = CFG_ENV,
-    minutes_to_eval: List[int] = DEFAULT_MINUTES_TO_EVAL_ETA,
 ):
 
     pipeline = make_pipeline(
@@ -95,7 +67,9 @@ def train_eta(
             ],
             verbose_feature_names_out=False,
         ),
-        PartitionedMetaEstimator(LGBMRegressor(n_estimators=20), partition_column),
+        PartitionedMetaEstimator(
+            LGBMRegressor(n_estimators=50, objective="regression_l1"), partition_column
+        ),
     )
     pipeline.set_output(transform="pandas")
 
@@ -111,6 +85,4 @@ def train_eta(
         mlflow_tracking_uri=mlflow_tracking_uri,
         test_size=test_size,
         env=env,
-        dataset_transformations=[postprocess_dataset_eta],
-        minutes_to_eval=minutes_to_eval,
     )
