ff090090bfc0de716d124cb1bbb2d3c1fc919c84
CrossNox <ijmermet@gmail.com>
2023-03-12 16:05:37 -0300

    fix metrics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.9.3] - 2023-03-12
+### Fixed
+- Normalize confusion matrix metrics
+
 ## [2.9.2] - 2023-03-07
 ### Fixed
 - Missing feature on predict
--- frame/train/metrics.py
+++ frame/train/metrics.py
@@ -1,29 +1,29 @@
 from sklearn.metrics import confusion_matrix
 
 
-def tn(y_true, y_pred):
+def tn(y_true, y_pred, normalize="all"):
     _tn, _, _, _ = confusion_matrix(
-        y_true, y_pred, labels=[0, 1], normalize=None
+        y_true, y_pred, labels=[0, 1], normalize=normalize
     ).ravel()
     return _tn
 
 
-def fp(y_true, y_pred):
+def fp(y_true, y_pred, normalize="all"):
     _, _fp, _, _ = confusion_matrix(
-        y_true, y_pred, labels=[0, 1], normalize=None
+        y_true, y_pred, labels=[0, 1], normalize=normalize
     ).ravel()
     return _fp
 
 
-def fn(y_true, y_pred):
+def fn(y_true, y_pred, normalize="all"):
     _, _, _fn, _ = confusion_matrix(
-        y_true, y_pred, labels=[0, 1], normalize=None
+        y_true, y_pred, labels=[0, 1], normalize=normalize
     ).ravel()
     return _fn
 
 
-def tp(y_true, y_pred):
+def tp(y_true, y_pred, normalize="all"):
     _, _, _, _tp = confusion_matrix(
-        y_true, y_pred, labels=[0, 1], normalize=None
+        y_true, y_pred, labels=[0, 1], normalize=normalize
     ).ravel()
     return _tp
